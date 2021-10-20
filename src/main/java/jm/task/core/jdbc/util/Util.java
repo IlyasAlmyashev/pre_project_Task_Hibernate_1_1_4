d4daa7a9bd61c36b9f67a74b1dd8cdc5972106f5
IlyasAlmyashev <ilyas.almyashev.1991@gmail.com>
2021-10-20 15:12:53 +0300

    выполнил задание

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,25 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String DATABASE = "jdbc:mysql://localhost:3306/test";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "60290root";
+    private static Connection connection;
+
+    private Util() {
+
+    }
+
+    public static Connection getConnection() {
+        try {
+            connection = DriverManager.getConnection(DATABASE, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
